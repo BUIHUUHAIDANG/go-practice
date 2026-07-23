44800e0510da134067da89003a79006b48b89ac2
BUIHUUHAIDANG <dangbui0408@gmail.com>
2026-07-23 15:23:46 +0700

    problem3513

---
+++ problem3513/NumberUniqueXor/main.go
@@ -0,0 +1,18 @@
+package main
+
+
+
+func uniqueXorTriplets(nums []int) int {
+	n := len(nums)
+	if n <= 2 {
+		return n
+	}
+	ans := 1
+	for ans <= n {
+		ans <<= 1
+	}
+	return ans
+}
+func main(){
+
+}
